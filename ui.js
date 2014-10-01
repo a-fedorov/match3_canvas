3b7da3e431b970433b85b0b626171f6a0c596e57
a-fedorov <alex.fedorov1991@gmail.com>
2014-10-01 17:19:40 +0300

    Make total refactor: delete ui object, write more simple code for dom manipulation

--- ui.js
+++ ui.js
@@ -1,177 +1,99 @@
-var ui = {
-  init: function () {
-    this.menuPage.startBtn.addEvent();
-    this.menuPage.optionsBtn.addEvent();
-    
-    this.gameModePage.normalMode.addEvent();
-    this.gameModePage.timeMode.addEvent();
-    this.gameModePage.endlessMode.addEvent();
-
-    this.gamePage.topPanel.backBtn.addEvent();
-  },
-
-  menuPage: {
-    element: document.querySelector('.menuPage'),
-    show: function () { ui.showElement(this.element); },
-    hide: function () { ui.hideElement(this.element); },
-
-    startBtn: {
-      element: document.querySelector('.start'),
-      addEvent: function () {
-        ui.addEvent(this.element, 'click', function(){
-          ui.gameModePage.show();
-          ui.menuPage.hide();
-        });
-      }
-    },
-
-    optionsBtn: {
-      element: document.querySelector('.options'),
-      addEvent: function () {
-        ui.addEvent(this.element, 'click', function(){
-          console.log('no options')
-        })
-      },
-    }
-    
-  },
-
-  gameModePage: {
-    element: document.querySelector('.gameModePage'),
-
-    show: function () { ui.showElement(this.element); },
-    hide: function () { ui.hideElement(this.element); },
-
-    normalMode: {
-      element: document.getElementById('normal'),
-      addEvent: function () {
-        ui.addEvent(this.element, 'click', function(){
-          ui.gamePage.show();
-          ui.gameModePage.hide();
-
-          ui.gamePage.bottomPanel.timer.hide();
-          ui.gamePage.bottomPanel.levelProgress.show();
-          startGame('normal');
-        })
-      },
-    },
-
-    timeMode: {
-      element: document.getElementById('time'),
-      addEvent: function () {
-        ui.addEvent(this.element, 'click', function(){
-          ui.gamePage.show();
-          ui.gameModePage.hide();
-
-          ui.gamePage.bottomPanel.timer.show();
-          ui.gamePage.bottomPanel.levelProgress.hide();
-          startGame('time');
-        })
-      }
-    },
-
-    endlessMode: {
-      element: document.getElementById('endless'),
-      addEvent: function () {
-        ui.addEvent(this.element, 'click', function(){
-          ui.gameModePage.hide();
-          ui.gamePage.show();
-          
-          ui.gamePage.bottomPanel.timer.hide();
-          ui.gamePage.bottomPanel.levelProgress.hide();
-          startGame('endless');
-        })
-      }      
-    }
-  },
-
-  gamePage: {
-    element: document.querySelector('.gamePage'),
-    
-    show: function(){ ui.showElement(this.element); },
-    hide: function(){ ui.hideElement(this.element); },
-
-    canvas: {
-      element: document.getElementById('board'),
-    },
-
-    topPanel: {
-      element: document.querySelector('.topPanel'),
-
-      scoreLabel: {
-        element: document.querySelector('.score'),
-        update: function(points){ this.element.innerHTML = 'Score: ' + points; },
-        reset: function(){ this.element.innerHTML = 'Score: 0'; }
-      },
-      
-      backBtn: {
-        element: document.querySelector('.exit'),
-        addEvent: function () {
-          ui.addEvent(this.element, 'click', function(){
-            ui.gamePage.hide();
-            ui.gameModePage.show();
-            ui.gamePage.topPanel.scoreLabel.reset();
-          })
-        }
-      }
-    },
-
-    bottomPanel: {
-      element: document.querySelector('.bottomPanel'),
-  
-      show: function() { ui.showElement(this.element); },
-      hide: function() { ui.hideElement(this.element); },
-
-      timer: {
-        element: document.querySelector('.indicator'),
-        container: document.querySelector('.time'),
-        show: function() { ui.showElement(this.container); },
-        hide: function() { ui.hideElement(this.container); },
-        update: function(percent) { this.element.style.width = percent + '%'; },
-        reset: function() { this.element.style.width = 100 + '%'}
-      },
-
-      levelProgress: {
-        element: document.querySelector('.progress'),
-        container: document.querySelector('.levelProgress'),
-        show: function() { ui.showElement(this.container); },
-        hide: function() { ui.hideElement(this.container); },
-      }
-    },
-  },
-
-  showElement: function (el) {
-    el.style.visibility = 'visible';
-    el.style.opacity = '1';
-  },
-
-  hideElement: function (el) {
-    el.style.visibility = 'hidden';
-    el.style.opacity = '0';
-  },
-
-  addEvent: function(el, eventType, eventFunction){
-    el.addEventListener('click', eventFunction, true);
-  },
+var screens = {
+  menuPage: document.getElementById('menuPage'),
+  gameModePage: document.getElementById('gameModePage'),
+  gamePage: document.getElementById('gamePage'),
+  gameOverPage: document.getElementById('gameOverPage'),
+  nextLevelPage: document.getElementById('nextLevelPage'),
 }
 
-  
-  var game;
-  
-  ui.init();
+var buttons = {
+  start: document.getElementById('start'),
+  back: document.getElementById('backToGameMode'),
+  backToMainMenu: document.getElementById('backToMainMenu'),
+  normalMode: document.getElementById('normal'),
+  timeMode: document.getElementById('time'),
+  endlessMode: document.getElementById('endless')
+}
+
+var panels = {
+  top: document.getElementById('topPanel'),
+  bottom: document.getElementById('bottomPanel'),
+}
+
+var progress = {
+  time: document.getElementById('timeProgress'),
+  level: document.getElementById('levelProgress'),
+}
+
+var indicators = {
+  time: document.getElementById('timeIndicator'),
+  level: document.getElementById('levelIndicator'),
+  score: document.getElementById('score'),
+}
+
+// Установка событий
+buttons.start.addEventListener('click', function(){
+  show(screens.gameModePage);
+  hide(screens.menuPage);
+}, false);
+
+buttons.back.addEventListener('click', function(){
+  show(screens.gameModePage);
+  hide(screens.gamePage);
+}, false);
+
+buttons.backToMainMenu.addEventListener('click', function(){
+  show(screens.menuPage);
+  hide(screens.gameModePage);
+}, false);
+
+buttons.normalMode.addEventListener('click', function(){
+  show(progress.level);
+  hide(progress.time);
+  startGame('normal');
+}, false);
+
+buttons.timeMode.addEventListener('click', function(){
+  show(progress.time);
+  hide(progress.level);
+  startGame('time');
+}, false);
+
+buttons.endlessMode.addEventListener('click', function(){
+  hide(panels.bottom);
+  startGame('endless');
+}, false);
 
 
-  function startGame(mode){
+function updateTimeIndicator(percent){
+  indicators.time.style.width = percent + '%';
+}
 
-    game = new Game(ui.gamePage.canvas.element)
-    game.start(mode);
+function updateLevelIndicator(percent){
+  indicators.level.style.width = percent + '%';
+}
 
-    // gremlinTest();
-  }
+function updateScoreLabel(points){
+  indicators.score.innerHTML = 'Score: ' + points;
+}
 
-  function gremlinTest(){
-    var horde = gremlins.createHorde().gremlin(gremlins.species.clicker().clickTypes(['click'])); 
-    horde.unleash();
-  }
 
-  // window.onload = startGame;
+var canvas = document.getElementById('board');
+var game = new Game(canvas);
+
+function startGame(mode){
+  show(screens.gamePage);
+  hide(screens.gameModePage);
+
+  game.start(mode);
+}
+
+function show(el){
+  el.style.opacity = 1;
+  el.style.visibility = 'visible';
+}
+
+function hide(el){
+  el.style.opacity = 0;
+  el.style.visibility = 'hidden';
+}
